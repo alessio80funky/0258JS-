0531d9351eee8343c1f4912af83d320a96e79ec1
alessio80funky <alessio80funky@gmail.com>
2026-06-30 11:45:52 +0900

    04/30 1145

--- javascript4/hikaku/code.js
+++ javascript4/hikaku/code.js
@@ -2,4 +2,25 @@
 
 //評価型「===, !== , ==, !=」
 
+/**
+ * == ->  大体同じ
+ * === ->  完全に同じ [実務]
+ * != -> 大体違う
+ * !== ->  完全に違う [実務]
+ */
+
+let x = 0 == false;
+console.log(x);//true
+
+let y = 0 === false;
+console.log(y);//false
+
+let j = 0 != false;
+console.log(j);//false
+
+let k = 0 !== false;
+console.log(k);//true
+
+
+
 //大小型「 <, >, <=, >=」
